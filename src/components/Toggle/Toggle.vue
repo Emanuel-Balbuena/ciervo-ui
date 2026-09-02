06dceab121f0aa2c684e106e33c57d1bb8a44717
Emanuel-Balbuena <emanuelbs2016@hotmail.com>
2026-09-02 13:14:49 -0700

    fix: import ref in Toggle.vue to prevent runtime mount error

--- src/components/Toggle/Toggle.vue
+++ src/components/Toggle/Toggle.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, inject } from 'vue';
+import { ref, computed, inject } from 'vue';
 import type { ComputedRef } from 'vue';
 import { play } from 'cuelume';
 
